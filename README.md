5adcdbac4d74650f7e362c8e91431e3f5e66ed4f
Andrew Bateman <gomezbateman@yahoo.com>
2020-08-11 16:47:04 +0200

    code formatting & readme updated

--- README.md
+++ README.md
@@ -1,51 +1,47 @@
-# TensorFlow Notepad
+# :zap: TensorFlow Notepad
 
-Code to experiment with machine learning, shuffling data using a seed phrase.
-
-Uses TensorFlow in javascript, following Udemy Course [Machine Learning With Javascript].
+* Code to experiment with machine learning, shuffling data using a seed phrase.
+* Uses TensorFlow in javascript, following Udemy Course [Machine Learning With Javascript].
 
 *** Note: to open web links in a new window use: _ctrl+click on link_**
 
-## Table of contents
+## :page_facing_up: Table of contents
 
-* [General info](#general-info)
-* [Screenshots](#screenshots)
-* [Technologies](#technologies)
-* [Setup](#setup)
-* [Features](#features)
-* [Status](#status)
-* [Inspiration](#inspiration)
-* [Contact](#contact)
+* [:zap: TensorFlow Notepad](#zap-tensorflow-notepad)
+  * [:page_facing_up: Table of contents](#page_facing_up-table-of-contents)
+  * [:books: General info](#books-general-info)
+  * [:camera: Screenshots](#camera-screenshots)
+  * [:signal_strength: Technologies](#signal_strength-technologies)
+  * [:floppy_disk: Setup](#floppy_disk-setup)
+  * [:computer: Code Examples](#computer-code-examples)
+  * [:cool: Features](#cool-features)
+  * [:clipboard: Status & To-Do List](#clipboard-status--to-do-list)
+  * [:clap: Inspiration](#clap-inspiration)
+  * [:envelope: Contact](#envelope-contact)
 
-## General info
+## :books: General info
 
-"_TensorFlow.js is an open-source hardware-accelerated JavaScript library for training and deploying machine learning models._"
+* "_TensorFlow.js is an open-source hardware-accelerated JavaScript library for training and deploying machine learning models._"
 
-## Screenshots
+## :camera: Screenshots
 
 ![Example screenshot](./img/.png)
 
-## Technologies
+## :signal_strength: Technologies
 
-* [TensorFlow.js v0.1.7](https://js.tensorflow.org/), an open-source hardware-accelerated JavaScript library for training and deploying machine learning models.
-
-* [@tensorflow/tfjs-node v1.1.2](https://www.npmjs.com/package/@tensorflow/tfjs-node).
-
-* [node.js v10.15.0 LTS](https://nodejs.org)
-
-* [Lodash v4.17.11](https://lodash.com/), a modern JavaScript utility library delivering modularity, performance & extras. It has lots of methods but is slow.
-
+* [TensorFlow.js](https://js.tensorflow.org/), an open-source hardware-accelerated JavaScript library for training and deploying machine learning models.
+* [@tensorflow/tfjs-node v2](https://www.npmjs.com/package/@tensorflow/tfjs-node).
+* [node.js v12 LTS](https://nodejs.org)
+* [Lodash v4](https://lodash.com/), a modern JavaScript utility library delivering modularity, performance & extras. It has lots of methods but is slow.
 * [Javascript playground](https://stephengrider.github.io/JSPlaygrounds/), a sandbox for writing javascript code and seeing the result on the right.
 
-## Setup
+## :floppy_disk: Setup
 
 * type `node load-csv.js` in console terminal to see any console.logs.
-
 * It was necessary to install Python on my PC for this to compile.
-
 * Use 'chrome://inspect/#devices' in browser to run in Chrome developer tools.
 
-## Code Examples
+## :computer: Code Examples
 
 * result of typing: `node load-csv.js`
 
@@ -56,20 +52,19 @@ testFeatures undefined
 testLabels undefined
 ```
 
-## Features
+## :cool: Features
 
 * to follow
 
-## Status & To-Do List
+## :clipboard: Status & To-Do List
 
-* Status:  returns Features and Labels but testFeatures and testLabels undefined - see Code Example
+* Status: returns Features and Labels but testFeatures and testLabels undefined - see Code Example
+* To-Do: Run again and correct data.
 
-* To-Do: correct data.
-
-## Inspiration
+## :clap: Inspiration
 
 Code from Udemy Course [Machine Learning With Javascript](https://www.udemy.com/machine-learning-with-javascript/learn/lecture/12355012#overview) section 183, 184.
 
-## Contact
+## :envelope: Contact
 
-Repo created by [ABateman](https://www.andrewbateman.org) - feel free to contact me!
+* Repo created by [ABateman](https://www.andrewbateman.org) - you are welcome to [send me a message](https://andrewbateman.org/contact)
--- load-csv.js
+++ load-csv.js
@@ -2,85 +2,85 @@ const fs = require('fs');
 const _ = require('lodash');
 
 // library to shuffle an array of records
-const shuffleSeed = require('shuffle-seed'); 
+const shuffleSeed = require('shuffle-seed');
 
 function extractColumns(data, columnNames) {
-  const headers = _.first(data);
+	const headers = _.first(data);
 
-  const indexes = _.map(columnNames, column => headers.indexOf(column));
-  const extracted = _.map(data, row => _.pullAt(row, indexes));
+	const indexes = _.map(columnNames, (column) => headers.indexOf(column));
+	const extracted = _.map(data, (row) => _.pullAt(row, indexes));
 
-  return extracted;
+	return extracted;
 }
 
 function loadCSV(
-  filename,
-  {
-    dataColumns = [],
-    labelColumns = [],
-    converters = {},
-    shuffle = true,
-    splitTest = false
-  }
+	filename,
+	{
+		dataColumns = [],
+		labelColumns = [],
+		converters = {},
+		shuffle = true,
+		splitTest = false,
+	}
 ) {
-  let data = fs.readFileSync(filename, { encoding: 'utf-8' });
+	let data = fs.readFileSync(filename, { encoding: 'utf-8' });
 
-  data = _.map(data.split('\n'), d => d.split(','));
-  data = _.dropRightWhile(data, val => _.isEqual(val, ['']));
+	data = _.map(data.split('\n'), (d) => d.split(','));
+	data = _.dropRightWhile(data, (val) => _.isEqual(val, ['']));
 
-  const headers = _.first(data);
+	const headers = _.first(data);
 
-  data = _.map(data, (row, index) => {
-    if (index === 0) {
-      return row;
-    }
-    return _.map(row, (element, index) => {
-      if (converters[headers[index]]) {
-        const converted = converters[headers[index]](element);
-        return _.isNaN(converted) ? element : converted;
-      }
+	data = _.map(data, (row, index) => {
+		if (index === 0) {
+			return row;
+		}
+		return _.map(row, (element, index) => {
+			if (converters[headers[index]]) {
+				const converted = converters[headers[index]](element);
+				return _.isNaN(converted) ? element : converted;
+			}
 
-      const result = parseFloat(element.replace('"', ''));
-      return _.isNaN(result) ? element : result;
-    });
-  });
+			const result = parseFloat(element.replace('"', ''));
+			return _.isNaN(result) ? element : result;
+		});
+	});
 
-  let labels = extractColumns(data, labelColumns);
-  data = extractColumns(data, dataColumns);
+	let labels = extractColumns(data, labelColumns);
+	data = extractColumns(data, dataColumns);
 
-  data.shift();
-  labels.shift();
+	data.shift();
+	labels.shift();
 
 	// shuffle and splitTest are booleans
-  if (shuffle) {
-    data = shuffleSeed.shuffle(data, 'phrase');
-    labels = shuffleSeed.shuffle(labels, 'phrase');
-  }
+	if (shuffle) {
+		data = shuffleSeed.shuffle(data, 'phrase');
+		labels = shuffleSeed.shuffle(labels, 'phrase');
+	}
 
-  if (splitTest) {
-    const trainSize = _.isNumber(splitTest)
-      ? splitTest
-      : Math.floor(data.length / 2);
+	if (splitTest) {
+		const trainSize = _.isNumber(splitTest)
+			? splitTest
+			: Math.floor(data.length / 2);
 
-    return {
-      features: data.slice(0, trainSize), // gives all data from train size to end of array
-      labels: labels.slice(0, trainSize),
-      testFeatures: data.slice(trainSize), // gives all data from 0 to trainsize
-      testLabels: labels.slice(trainSize)
-    };
-  } else {
-    return { features: data, labels };
-  }
+		return {
+			features: data.slice(0, trainSize), // gives all data from train size to end of array
+			labels: labels.slice(0, trainSize),
+			testFeatures: data.slice(trainSize), // gives all data from 0 to trainsize
+			testLabels: labels.slice(trainSize),
+		};
+	} else {
+		return { features: data, labels };
+	}
 }
 // destructuring used below
 const { features, labels, testFeatures, testLabels } = loadCSV('data.csv', {
-  dataColumns: ['height', 'value'],
-  labelColumns: ['passed'],
-  shuffle: true,
-  splitTest: false,
-  converters: {
-    passed: val => (val === 'TRUE' ? 1 : 0)
-  }
+	dataColumns: ['height', 'value'],
+	labelColumns: ['passed'],
+	shuffle: true,
+	splitTest: false,
+	converters: {
+		passed: (val) => (val === 'TRUE' ? 1 : 0),
+	},
 });
 
 console.log('Features', features);
